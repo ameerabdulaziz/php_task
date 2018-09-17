2600b30a407649f4c6774c3ffe76d3d5156ae85b
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-17 18:14:02 +0200

    Set flash messages

--- form.php
+++ form.php
@@ -18,18 +18,36 @@ if (isset($_POST['submit'])):
     $file_name = $file['name'];
     $ext = pathinfo(strtolower($file_name), PATHINFO_EXTENSION);
     if ($ext !== 'csv'):
-
+        $_SESSION['error'] = 'Sorry, CSV file is the only accepted!';
+        header('Location: index.php');
+        return;
     else:
         $file_location = $file['tmp_name'];
         $customer_names = get_customer_info($file_location)[0];
         $customer_phones = get_customer_info($file_location)[1];
-        print_r($customer_names);
-        print_r($customer_phones);
+        $_SESSION['success'] = 'Message sent!';
+        header('Location: index.php');
+        return;
     endif;
 endif;
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
+    <?php if (isset($_SESSION['error'])):?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <?= $_SESSION['error'] ?><?php unset($_SESSION['error']); ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php elseif (isset($_SESSION['success'])): ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <?= $_SESSION['success'] ?><?php unset($_SESSION['success']); ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif;?>
     <div class="form-group">
         <label for="csv_file">Upload a CSV file:</label>
         <input type="file" class="form-control-file" id="csv_file" name="csv_file">
