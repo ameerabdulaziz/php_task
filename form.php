fdad2e0b1b53fb43f2b562cd3ec358450ea7e176
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-17 14:18:24 +0200

    Create a form

--- form.php
+++ form.php
@@ -1,2 +1,12 @@
-<?php
-echo 'form';
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="csv_file">Upload a CSV file:</label>
+        <input type="file" class="form-control-file" id="csv_file" name="csv_file">
+    </div>
+    <div class="form-group">
+        <label for="message">Message:</label>
+        <textarea class="form-control-file" name="message" id="message" cols="30" rows="10"></textarea>
+    </div>
+
+    <button class="btn btn-success" type="submit">Send</button>
+</form>
